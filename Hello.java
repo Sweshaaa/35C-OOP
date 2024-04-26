5b1ec30cc4403151c60d03728241505391fedeec
Swesha Tamang <sweshatamang58@gmail.com>
2024-04-26 11:59:10 +0545

    week1

---
+++ Hello.java
@@ -0,0 +1,74 @@
+public class Hello{
+    public static void main(String[] args) {
+        
+    /* Write a program that prints Hello World! to the console. Save the program as Hello.java. */
+    System.out.println("Hello World!!");
+
+
+/*This is my first program
+
+I am on module ST4003CEM
+
+System.out.print (not println) and produces the same output. */
+System.out.print("This is myy first program \nI am on module ST4003CEM");      
+
+
+/*A "quoted" String is
+
+'much' better if you learn
+
+the rules of "escape sequences."
+
+Also, "" represents an empty
+
+String. Don't forget: use \"
+
+instead of " !
+
+'' is not the same as " */
+System.out.println("A \"quoted\" String is \n\'much\' better if you learn \nthe rules of \"escape sequences.\" \nAlso, \"\" represents an empty \nString. Don't forget: use \\\" \ninstead of \" !\n \" is not the same as \"");
+
+
+/**
+
+**
+
+***
+
+**** */
+
+System.out.println("*");
+System.out.println("**");
+System.out.println("***");
+System.out.println("****");
+
+/**********
+
+*********
+
+*********
+
+********* */
+System.out.println("**********");
+System.out.println("**********");
+System.out.println("**********");
+System.out.println("**********");
+
+
+/*A well-formed Java program has a main method with { and } braces.
+
+
+
+A System.out.println statement has ( and ) and usually a String that starts and ends with a " character.
+
+(But we type \" instead!)
+
+What is the difference between
+
+a ' and a "?  Or between a " and a \"? */ 
+
+System.out.println("A well-formed Java program has a main method with { and } braces.\n");
+System.out.println("A System.out.println statement has ( and ) and usually a String \nthat starts and ends with a \" character. \n(But we type \\\" instead!) \nWhat is the difference between \na \' and a \"?  Or between a \" and a \\\"?\n ");
+System.out.println("One is what we see when we're typing our program. The other is what appears on the \"console.\"");
+}
+}
